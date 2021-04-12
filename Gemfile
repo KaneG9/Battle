f0b7ae8846110f713b1843bed000d856e9ea1869
dominicmeddick <dominicmeddick@gmail.com>
2021-04-12 15:17:13 +0100

    Start and restart

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ group :development, :test do
 
 gem 'sinatra', '~> 2.1'
 gem 'webrick', '~> 1.7'
+gem 'sinatra-reloader', '~> 1.0'
 
 end
--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
 
 get "/" do
   "hello!"
+end
+
+get "/secret" do
+  "Dirty little secret"
+end
+
+get "/kanesecret" do
+  "Its a hard one"
+end
+
+get "/domsecret" do
+  "Pinapple and Pizza, yum"
 end
